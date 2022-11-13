25bdfb03f6bc29d00778cfc7b3fc070028a0e35e
hacchaa <jomwit22@gmail.com>
2022-11-14 01:00:45 +0700

    add thumbnail image feature
    documented steps is in shop_controller's main function

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -8,12 +8,27 @@ class ShopController < ApplicationController
     @stock = Array.new
     @seller = Array.new
     @id = Array.new
+    @thumbnail = Array.new  # for displaying image
     @all_item = Item
     @all_item.all.each do |all_item|
       @id.push(all_item.id)
       @name.push(all_item.name)
       @price.push(all_item.price)
       @stock.push(all_item.stock)
+      if all_item.picture.attached? # check if there's an image attached
+        @thumbnail.push(all_item.picture.variant(:thumb)) # if yes use that item
+        # steps to make this work
+        # 1 add these two lines in Gemfile uncomment 
+          # gem "image_processing", "~> 1.2"
+          # gem "ruby-vips"
+        # 2 in wsl terminal run
+          # bundle install
+          # sudo apt-get update
+          # sudo apt install libvips
+        # 3 rails s then should work
+        else
+        @thumbnail.push(:nopic)  # if no insert "nopic"
+      end
       who = User.find_by id: all_item.User_id
       @seller.push(who.username)
     end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :User
+  has_one_attached :picture do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
 end
